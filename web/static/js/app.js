858c5463205331deda26c84885dfcf7c02a03ada
Brent Yates <byates@vendasta.com>
2025-11-02 19:51:32 -0600

    Remove click-to-expand functionality from status bar in app.js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -3,12 +3,4 @@ import { SquareGolfApp } from './core/SquareGolfApp.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     window.app = new SquareGolfApp();
-
-    // Status bar click-to-expand functionality
-    const statusBar = document.getElementById('statusBar');
-    if (statusBar) {
-        statusBar.addEventListener('click', () => {
-            statusBar.classList.toggle('expanded');
-        });
-    }
 });
