a5afbaf1b69fff229fddf7f7eb3555b27df67e45
Brent Yates <byates@vendasta.com>
2026-03-05 23:06:14 -0600

    Bump release version to v0.1.1

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	// Version is the current version of the application
-	Version = "0.1.0-alpha.12"
+	Version = "0.1.1"
 
 	// BuildTime is the time the binary was built
 	BuildTime = "unknown"
